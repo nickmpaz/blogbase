5f2c54982bdfb63afac8691d8556ffe614ab8709
Nick Paz <nicholasmpaz@gmail.com>
2020-02-15 20:20:46 -0800

    initial commit

--- bloggy.py
+++ bloggy.py
@@ -1,3 +1,3 @@
 from subprocess import check_output
-out = check_output(["git", "rev-list", "master", "--reverse"])
+out = check_output(["git", "rev-list", "master", "--reverse"]).decode('utf-8')
 print(out)
