0668c65626a357d00fa5835ed4a7155aaf2a5f2e
Nick Paz <nicholasmpaz@gmail.com>
2020-02-16 11:40:48 -0800

    changes

--- bloggy.py
+++ bloggy.py
@@ -1,3 +1,11 @@
 from subprocess import check_output
-out = check_output(["git", "rev-list", "master", "--reverse"]).decode('utf-8')
-print(out)
+
+
+commits_raw = check_output(["git", "rev-list", "master", "--reverse"]).decode('utf-8')
+commits = commits_raw.split('\n')[:-1]
+for commit in commits:
+    commit_message = check_output(["git", "show", "-s", "--format=%B", commit]).decode('utf-8')
+    changed_files_raw = check_output(["git", "diff-tree", "-r", "--no-commit-id", "--name-only", commit]).decode('utf-8')
+    changed_files = changed_files_raw.split('\n')[:-1]
+    print(changed_files_raw)
+# print(commits)
